cc5a087c89a20395fef54930f5bb99d5487dd8f1
Roharx <balint.imal@gmail.com>
2024-05-29 22:24:34 +0200

    fix/updated-whitelist-for-test-and-deplyoment

--- api/Config/Whitelist.cs
+++ api/Config/Whitelist.cs
@@ -5,6 +5,8 @@ public class Whitelist
     public static List<string> AllowedUrls { get; } = new List<string>
     {
         "http://localhost:4200/",
-        "http://localhost:5296/"
+        "http://localhost:5296/",
+        "http://161.97.92.174/",
+        "http://161.97.92.158/"
     };
 }
