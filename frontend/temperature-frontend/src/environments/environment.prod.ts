6ce5b16749c9ab2fe2b4b82a7cb07e5fd6a84158
Roharx <balint.imal@gmail.com>
2024-05-30 02:02:02 +0200

    fix/cors-again

--- frontend/temperature-frontend/src/environments/environment.prod.ts
+++ frontend/temperature-frontend/src/environments/environment.prod.ts
@@ -1,4 +1,4 @@
 export const environment = {
   production: true,
-  baseURL: 'http://161.97.92.174/api'
+  baseURL: 'http://161.97.92.174'
 };
--- frontend/temperature-frontend/src/environments/environment.ts
+++ frontend/temperature-frontend/src/environments/environment.ts
@@ -4,7 +4,7 @@
 
 export const environment = {
   production: false,
-  baseURL: 'http://161.97.92.174/api'
+  baseURL: 'http://161.97.92.174'
 };
 
 /*
